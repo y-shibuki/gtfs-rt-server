21b273c8c6c1205e0f2510c21b04007a3a5cf272
Hinata Yoshida <12867630+y-shibuki@users.noreply.github.com>
2023-10-17 14:33:29 +0900

    fix

--- main.sh
+++ main.sh
@@ -6,7 +6,9 @@ source ./.env.local
 
 export PYTHONPATH=./app:$PYTHONPATH
 
-if [ -e "./app/src/$1.py" ]; then
+if [ $1 = "start_api" ]; then
+    uvicorn --app-dir ./app/src api:app --reload
+elif [ -e "./app/src/$1.py" ]; then
     python3 "./app/src/$1.py"
 else
     echo "コマンドが不明です"
