c04ff674d9fb5572bf1615b7f4eb8e16175cc2f4
Hinata Yoshida <12867630+y-shibuki@users.noreply.github.com>
2023-10-23 12:17:15 +0900

    CORSへの対応

--- app/src/api.py
+++ app/src/api.py
@@ -1,10 +1,23 @@
 import pandas as pd
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.utils.db import get_db_adapter
 from app.utils.logger import getLogger
 
 app = FastAPI()
+
+app = FastAPI()
+
+# CORSを回避するために追加（今回の肝）
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,   # 追記により追加
+    allow_methods=["*"],      # 追記により追加
+    allow_headers=["*"]       # 追記により追加
+)
+
 logger = getLogger(__name__)
 db_adapter = get_db_adapter()
 
